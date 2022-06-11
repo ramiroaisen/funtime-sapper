2f0608ff717e4d26ea81e796d194a7afbf8a79db
Ramiro Aisen <ramiroaisen@gmail.com>
2022-06-11 16:49:02 -0300

    fix: mime lookup

--- runtime/src/server/middleware/index.ts
+++ runtime/src/server/middleware/index.ts
@@ -112,7 +112,7 @@ export function serve({ prefix, pathname, cache_control }: {
 
 	return (req: SapperRequest, res: SapperResponse, next: () => void) => {
 		if (filter(req)) {
-			const type = mime.getType(req.path);
+			const type = mime.lookup(req.path);
 
 			try {
 				const file = path.posix.normalize(decodeURIComponent(req.path));
